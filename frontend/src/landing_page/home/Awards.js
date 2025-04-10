d7f0ec617c3fb75f187dbb2bea37fa3e9ef6865c
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 11:03:38 +0530

    Update Awards.js

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,55 +2,53 @@ import React from 'react'
 
 function Awards() {
   return (
-    <div className='container mt-5'>
-      <div className='row'>
-        <div className='col-6 p-5'>
-          <img src='media\images\largestBroker.svg'
-             alt='Largest Broker'/>
-
+    <div className="container mx-auto mt-10 px-5">
+      <div className="flex flex-col md:flex-row items-center">
+        
+        {/* Left Image Section */}
+        <div className="w-full md:w-1/2 p-5 flex justify-center">
+          <img 
+            src="media/images/largestBroker.svg" 
+            alt="Largest Broker" 
+            className="w-full max-w-xs md:max-w-md"
+          />
         </div>
-        <div className='col-6 p-5 mt-5'>
-        <h1>Largest stock broker in India</h1>
-          <p className="mb-5">
+
+        {/* Right Content Section */}
+        <div className="w-full md:w-1/2 p-5 mt-5 md:mt-10">
+          <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
+            Largest stock broker in India
+          </h1>
+          <p className="my-5 text-center md:text-left">
             2+ million Zerodha clients contribute to over 15% of all retail
             order volumes in India daily by trading and investing in:
           </p>
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li>
-                  <p>Futures and Options</p>
-                </li>
-                <li>
-                  <p>Commodity derivatives</p>
-                </li>
-                <li>
-                  <p>Currency derivatives</p>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  <p>Stocks & IPOs</p>
-                </li>
-                <li>
-                  <p>Direct mutual funds</p>
-                </li>
-                <li>
-                  <p>Bonds and Govt. Securities</p>
-                </li>
-              </ul>
-            </div>
-            </div>
-            <img src="media/images/pressLogos.png" style={{ width: "90%" }} />
 
+          <div className="flex">
+            <ul className="w-1/2 list-disc pl-5">
+              <li>Futures and Options</li>
+              <li>Commodity derivatives</li>
+              <li>Currency derivatives</li>
+            </ul>
+            <ul className="w-1/2 list-disc pl-5">
+              <li>Stocks & IPOs</li>
+              <li>Direct mutual funds</li>
+              <li>Bonds and Govt. Securities</li>
+            </ul>
+          </div>
+
+          <div className="mt-8 flex justify-center md:justify-start">
+            <img 
+              src="media/images/pressLogos.png" 
+              alt="Press Logos" 
+              className="w-11/12 max-w-lg"
+            />
+          </div>
         </div>
-
       </div>
-      
     </div>
   )
 }
 
 export default Awards
+
