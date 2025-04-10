e91765a712cd05cce60ff446e21bbada2129babb
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 12:24:46 +0530

    Update Apps.js

--- dashboard/src/components/Apps.js
+++ dashboard/src/components/Apps.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Apps = () => {
   return (
-    <div className="app-container">
-      <h1 className="app-heading">Get it on Google</h1>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Get it on Google</h1>
       <img 
         src="https://zerodha.com/z-connect/wp-content/uploads/2017/05/google_play_badge.png" 
         alt="Google Play Badge" 
-        className="app-img" 
+        style={{ width: "200px", marginTop: "10px" }} 
       />
     </div>
   );
