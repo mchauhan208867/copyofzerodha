a853dc415b8558f61f464be99d005ff25d19d894
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 10:55:30 +0530

    Update Hero.js

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -14,7 +14,15 @@ function Hero() {
         <h1 className="mt-5">Invest in everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, and
         more</p>
-        <button style={{width: "20%", margin: "0 auto"}} className='p-2 btn btn-primary fs-5 mb-5' onClick={() => navigate("/signup")} >SignUp</button>
+        <div className="d-flex justify-content-center mb-5">
+  <button
+    className="btn btn-primary p-2 fs-5"
+    onClick={() => navigate("/signup")}
+  >
+    SignUp
+  </button>
+</div>
+
 
       </div>
       
